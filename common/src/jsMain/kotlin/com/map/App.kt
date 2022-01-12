0da00bcf27e3a58bef55a83e97bf5ebede21e220
avdim <avdim@mail.ru>
2022-01-12 15:09:06 +0300

    ComposeCounterApp

--- common/src/jsMain/kotlin/com/map/App.kt
+++ common/src/jsMain/kotlin/com/map/App.kt
@@ -2,30 +2,78 @@ package com.map
 
 import kotlinx.browser.document
 import org.w3c.dom.HTMLElement
-//import org.jetbrains.compose.web.renderComposable
-//import org.jetbrains.compose.demo.falling.views.fallingBalls
-//import org.jetbrains.compose.demo.falling.Game
-//import androidx.compose.runtime.remember
-//import kotlinx.browser.window
-//import org.jetbrains.compose.web.css.Style
-//import org.jetbrains.compose.web.ui.Styles
-
-//class JsGame : Game() {
-//    override fun now() = window.performance.now().toLong()
-//}
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
 
 fun main() {
     println("Hello Compose Web")
     val root = document.getElementById("root") as HTMLElement
     root.textContent = "Hello Compose Web"
-//    renderComposable(root = root) {
-//        Style(Styles)
-//        val game = remember {
-//            JsGame().apply {
-//              width = root.offsetWidth
-//              height = root.offsetHeight
-//            }
-//        }
-//        fallingBalls(game)
-//    }
+    ComposeCounterApp("root")
+}
+
+@JsExport
+abstract class ComposeCounterAppController {
+    abstract fun setCount(newCount: Int)
+    abstract fun dispose()
+}
+
+/**
+ * @param rootId - an id of an HTML element which is already added into the DOM.
+ * Compose will manage the content of this element.
+ *
+ * @param onCountChange - a callback to receive state updates in non-compose code.
+ *
+ * @return instance of [ComposeCounterAppController] to control the composition in non-compose code.
+ */
+@JsExport
+fun ComposeCounterApp(rootId: String, onCountChange: (Int) -> Unit = {}): ComposeCounterAppController {
+    var count: Int by mutableStateOf(0)
+
+    val composition = renderComposable(rootElementId = rootId) {
+        Counter(count) {
+            count = it
+            onCountChange(count)
+        }
+    }
+
+    return object : ComposeCounterAppController() {
+        override fun setCount(newCount: Int) {
+            count = newCount
+        }
+
+        override fun dispose() {
+            composition.dispose()
+        }
+    }
+}
+
+@Composable
+private fun Counter(count: Int, onCountChange: (Int) -> Unit) {
+    Div({ style { padding(25.px) } }) {
+        Button(attrs = {
+            onClick {
+                onCountChange(count - 1)
+            }
+        }) {
+            Text("-")
+        }
+
+        Span({ style { padding(15.px) } }) {
+            Text("$count")
+        }
+
+        Button(attrs = {
+            onClick {
+                onCountChange(count + 1)
+            }
+        }) {
+            Text("+")
+        }
+    }
 }
