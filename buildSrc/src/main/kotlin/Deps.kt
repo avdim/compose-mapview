2d7a123f354230a89551cc7c2628dbb9bb525d76
avdim <avdim@mail.ru>
2022-01-13 16:31:08 +0300

    remove redundant dependencies

--- buildSrc/src/main/kotlin/Deps.kt
+++
@@ -1,7 +0,0 @@
-object Deps {
-//    const val KTOR_VERSION = "1.4.1"
-    const val KTOR_VERSION = "1.6.7"
-//    const val KTOR_VERSION = "2.0.0-beta-1"
-
-    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0"
-}
