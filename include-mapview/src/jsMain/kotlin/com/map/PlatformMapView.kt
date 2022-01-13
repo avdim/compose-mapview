b271c0810e75ec4cc708cf025c0a91b80e7b4ebb
avdim <avdim@mail.ru>
2022-01-13 22:20:41 +0300

    onWheel

--- include-mapview/src/jsMain/kotlin/com/map/PlatformMapView.kt
+++ include-mapview/src/jsMain/kotlin/com/map/PlatformMapView.kt
@@ -5,10 +5,10 @@ import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 internal actual fun PlatformMapView() {
-    val store = createStore(0) { s: Int, intent: Int ->
+    val store = createStore(0.0) { s, intent: Double ->
         s + intent
     }
-    val stateFlow: StateFlow<Int> = store.stateFlow
+    val stateFlow: StateFlow<Double> = store.stateFlow
     LibJSCounter(stateFlow) {
         store.send(it)
     }
--- include-ui-browser/src/jsMain/kotlin/com/map/LibCounter.kt
+++ include-ui-browser/src/jsMain/kotlin/com/map/LibCounter.kt
@@ -3,6 +3,7 @@ package com.map
 import androidx.compose.runtime.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.dom.clear
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.dom.Text
@@ -12,12 +13,12 @@ import org.w3c.dom.HTMLCanvasElement
 
 @JsExport
 @Composable
-public fun LibJSCounter(stateFlow: StateFlow<Int>, sendIntent: (Int) -> Unit) {
-    val state: State<Int> = stateFlow.collectAsState()
+public fun LibJSCounter(stateFlow: StateFlow<Double>, sendIntent: (Double) -> Unit) {
+    val state: State<Double> = stateFlow.collectAsState()
     Div({ style { padding(25.px) } }) {
         Button(attrs = {
             onClick {
-                sendIntent(1)
+                sendIntent(1.0)
             }
         }) {
             Text("+++")
@@ -29,36 +30,52 @@ public fun LibJSCounter(stateFlow: StateFlow<Int>, sendIntent: (Int) -> Unit) {
 
         Button(attrs = {
             onClick {
-                sendIntent(-1)
+                sendIntent(-1.0)
             }
         }) {
             Text("---")
         }
     }
     Div {
-        CanvasWithRect(stateFlow)
+        CanvasWithRect(stateFlow) {
+            sendIntent(it)
+        }
     }
 }
 
 @Composable
-private fun CanvasWithRect(stateFlow: StateFlow<Int>) {
+private fun CanvasWithRect(stateFlow: StateFlow<Double>, onWheel:(Double)->Unit) {
     val state = stateFlow.collectAsState()
     TagElement(
         elementBuilder = ElementBuilder.createBuilder("canvas"),
         applyAttrs = {
-            val width = 200 + state.value * 10
-//            attr("width", "${width}px")
             attr("width", "200px")
             attr("height", "200px")
             attr("workaround", state.value.toString())
         },
         content = {
+            DisposableRefEffect { element: Element ->
+                val canvas = element as HTMLCanvasElement
+                canvas.onwheel = {
+                    it.preventDefault()//cancel page scrolling
+                    onWheel(it.deltaY / 10)
+                }
+                canvas.onmousemove = {
+//                    mouseMoveX(it.clientX)
+                    it.y
+                }
+                onDispose {
+                    //clear mouse handlers
+                    canvas.onmousemove = {}
+                    canvas.onwheel = {}
+                    println("canvas dispose")
+                }
+            }
             DomSideEffect(state.value) { element: Element ->
                 val canvas = element as HTMLCanvasElement
                 val ctx = canvas.getContext("2d") as CanvasRenderingContext2D
                 ctx.fillStyle = "green";
-                val size = 20.0 + state.value * 5
-                ctx.fillRect(0.0, 0.0, size, size)
+                ctx.fillRect(state.value.toDouble(), 0.0, 20.0, 20.0)
             }
         }
     )
