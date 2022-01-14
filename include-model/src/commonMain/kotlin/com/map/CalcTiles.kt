e5219de0d40fadec275cb49d91f174271f82c9c9
avdim <avdim@mail.ru>
2022-01-14 13:21:27 +0300

    zoom

--- include-model/src/commonMain/kotlin/com/map/CalcTiles.kt
+++ include-model/src/commonMain/kotlin/com/map/CalcTiles.kt
@@ -1,16 +1,46 @@
 package com.map
 
+import kotlin.math.log
+import kotlin.math.pow
+
 fun calcTiles(mapState: MapState, width: Int, height: Int): TilesGrid {
-    val tilesX = 3
-    val tilesY = 4
-    val zoomLevel = (mapState.zoom * MAX_ZOOM_LEVEL).toInt() + maxOf(tilesX, tilesY) - 1
-    val size = 200
+    val center: GeoPt = GeoPt(0.5, 0.5)
+    fun GeoPt.toDisplay(): Pt {
+        TODO()
+    }
+
+    fun Int.toGeo():Double {
+        val scale:Double = 1.0 + (mapState.zoom * 100)
+        return (this) / (scale * height)
+    }
+
+    fun Pt.toGeo(): GeoPt {
+        center
+        return GeoPt(
+            x.toGeo(),
+            y.toGeo()
+        )
+    }
+
+    val topLeftGeo = Pt(0, 0).toGeo()
+    val bottomRightGeo = Pt(width, height).toGeo()
+    val geoSize = bottomRightGeo delta topLeftGeo
+
+    val targetZoom = 1.0 / minOf(1.0, maxOf(geoSize.x, geoSize.y)) // 1 .. +Inf
+    val zoomLevel = calcZoomLevel(targetZoom)
+    val tileScale = targetZoom / calcZoom(zoomLevel)
+    val sizePx = (TILE_SIZE * tileScale).toInt()
+
+    val tilesX = (geoSize.x / TILE_SIZE.toGeo()).toInt() + 1
+    val tilesY = (geoSize.y / TILE_SIZE.toGeo()).toInt() + 1
+    val zoomLevel2 = (mapState.zoom * MAX_ZOOM_LEVEL).toInt() + maxOf(tilesX, tilesY) - 1
+
     val grid: List<List<DisplayTile>> = buildList {
         for (x in 0 until tilesX) {
             add(buildList {
                 for (y in 0 until tilesY) {
                     val tile = Tile(zoomLevel, x, y)
-                    add(DisplayTile(size, x * size, y * size, tile))
+                    add(DisplayTile(sizePx, x * sizePx, y * sizePx, tile))
                 }
             })
         }
@@ -25,4 +55,26 @@ fun calcTiles(mapState: MapState, width: Int, height: Int): TilesGrid {
             listOf(DisplayTile(200, 0, 200, Tile(1, 0, 1)), DisplayTile(200, 200, 200, Tile(1, 1, 1))),
         )
     )
+}
+
+data class GeoPt(val x: Double, val y: Double)
+data class Pt(val x: Int, val y: Int)
+
+infix fun GeoPt.delta(minus: GeoPt): GeoPt {
+    return GeoPt(x - minus.x, y - minus.y)
+}
+fun calcZoomLevel(zoom: Double):Int {
+    return log(zoom, 2.0).toInt() + 1
+    // 1.0 -> 1
+    // 2.0 -> 1
+    // 4.0 -> 2
+    // 8.0 -> 3
+}
+fun calcZoom(zoomLevel:Int):Double {
+    return 2.0.pow(zoomLevel)
+    when(zoomLevel) {
+        0 -> 1.0
+        1 -> 2.0
+        2 -> 4.0
+    }
 }
