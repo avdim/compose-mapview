c307bf166569c5a3c65a3b0d5866031bdc3ac6a6
avdim <avdim@mail.ru>
2022-01-19 19:53:40 +0300

    add calc tile tests

--- unit-tests/src/commonTest/kotlin/com/map/CalcTilesTest.kt
+++ unit-tests/src/commonTest/kotlin/com/map/CalcTilesTest.kt
@@ -7,10 +7,16 @@ class CalcTilesTest {
 
     @Test
     fun testSingleTile() {
-        val state = MapState<Unit>(512, 512, 1.0)
+        val state = MapState<Unit>(512, 512)
         val tiles = state.calcTiles()
         assertEquals(1, tiles.size)
     }
 
-}
+    @Test
+    fun test2x2TilesWithZoom() {
+        val state = MapState<Unit>(1024, 1024)
+        val tiles = state.calcTiles()
+        assertEquals(4, tiles.size)
+    }
 
+}
