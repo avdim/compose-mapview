ad6e15bbd57807663b85860bba00b45aef5ceed0
avdim <avdim@mail.ru>
2022-01-19 11:49:24 +0300

    cleanup android

--- include-ui-android-desktop/src/commonMain/kotlin/com/map/ImageUtils_common.kt
+++ include-ui-android-desktop/src/commonMain/kotlin/com/map/ImageUtils_common.kt
@@ -4,6 +4,5 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 
-expect fun ImageBitmap.toByteArray(): ByteArray
 expect fun ByteArray.toImageBitmap(): ImageBitmap
 expect fun GpuOptimizedImage.extract():ImageBitmap
--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 #shopt -s inherit_errexit
 
-./gradlew unit-tests:test unit-tests:check
+./gradlew unit-tests:test
+#./gradlew unit-tests:check
 ./gradlew sample-android:assembleDebug
 ./gradlew sample-browser:jsBrowserWebpack
 ./gradlew sample-desktop:run
