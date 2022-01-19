537c4c0f4c8478c8de5a309b985ef96b30321199
avdim <avdim@mail.ru>
2022-01-19 19:27:31 +0300

    remove unused code

--- include-mapview/src/jsMain/kotlin/com/map/CreateTilesRepository_browser.kt
+++ include-mapview/src/jsMain/kotlin/com/map/CreateTilesRepository_browser.kt
@@ -3,7 +3,14 @@ package com.map
 import androidx.compose.runtime.Composable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.await
+import org.w3c.dom.Image
+import org.w3c.dom.ImageBitmap
+import org.w3c.files.Blob
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlin.js.Promise
 
 @Composable
 internal actual fun createTilesRepository(
@@ -16,4 +23,28 @@ internal actual fun createTilesRepository(
 }
 
 internal actual fun getDispatcherIO(): CoroutineContext = Dispatchers.Default
+
+private fun createRealRepository(mapTilerSecretKey: String) =
+    object : ContentRepository<Tile, ImageBitmap> {
+        @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+        override suspend fun loadContent(tile: Tile): ImageBitmap {
+            val promise: Promise<ImageBitmap> = suspendCoroutine { continuation ->
+                val img = Image() // Create new <img> element
+                img.onload = {
+                    continuation.resume(
+                        createImageBitmap(img)
+                    )
+                }
+                img.src = Config.createTileUrl(tile.zoom, tile.x, tile.y, mapTilerSecretKey)
+            }
+            return promise.await()
+        }
+    }
+
+/**
+ * default JS browser API to convert <img> to ImageBitmap
+ * https://developer.mozilla.org/en-US/docs/Web/API/createImageBitmap
+ */
+external fun createImageBitmap(data: Image): Promise<ImageBitmap>
+external fun createImageBitmap(data: Blob): Promise<ImageBitmap>
 
--- include-model/src/desktopMain/kotlin/com/map/Time_desktop.kt
+++ include-model/src/desktopMain/kotlin/com/map/Time_desktop.kt
@@ -1,5 +1,3 @@
 package com.map
 
-import java.util.concurrent.ConcurrentHashMap
-
 actual inline fun timeMs(): Long = System.currentTimeMillis()
