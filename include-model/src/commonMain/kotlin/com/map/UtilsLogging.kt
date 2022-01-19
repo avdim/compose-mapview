15f14d23667563e5292bfd8b5f7db2fc3ffa99f6
avdim <avdim@mail.ru>
2022-01-19 18:27:14 +0300

    telemetry

--- include-model/src/commonMain/kotlin/com/map/UtilsLogging.kt
+++ include-model/src/commonMain/kotlin/com/map/UtilsLogging.kt
@@ -4,7 +4,7 @@ fun MapState<*>.toShortString(): String = buildString {
     appendLine("width: $width, height: $height")
     appendLine("scale: ${scale.toShortString()}")
     appendLine("zoom: $zoom")
-    appendLine("lat: ${centerGeo.latitude}, lon: ${centerGeo.longitude}")
+    appendLine("lat: ${centerGeo.latitude.toShortString()}, lon: ${centerGeo.longitude.toShortString()}")
 }
 
 fun Double.toShortString(significantDigits: Int = 5): String {
