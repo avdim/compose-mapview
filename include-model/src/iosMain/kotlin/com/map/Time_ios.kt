4564d253325d5e5e6439b60426336b31e8935039
avdim <avdim@mail.ru>
2022-01-23 13:28:26 +0300

    move timeMs to specific platforms

--- include-model/src/iosMain/kotlin/com/map/Time_ios.kt
+++
@@ -1,5 +0,0 @@
-package com.map
-
-import platform.Foundation.NSDate
-
-actual inline fun timeMs(): Long = 0 //todo redundant in iOS
---
+++ include-ui-browser/src/jsMain/kotlin/com/map/Time_js.kt
@@ -0,0 +1,3 @@
+package com.map
+
+inline fun timeMs(): Long = kotlin.js.Date.now().toLong()
