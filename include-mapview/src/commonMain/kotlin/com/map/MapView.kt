def5bf87f3ba24683587bfc8a684b3dddd227814
avdim <avdim@mail.ru>
2022-01-24 20:00:16 +0300

    try igordmn suggestion

--- include-mapview/src/commonMain/kotlin/com/map/MapView.kt
+++ include-mapview/src/commonMain/kotlin/com/map/MapView.kt
@@ -1,13 +1,16 @@
 package com.map
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
+
+data class ExternalState(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val scale: Double = 1.0,
+)
 
 /**
  * MapView to display Earth tile maps. API provided by cloud.maptiler.com
@@ -38,19 +41,40 @@ import kotlinx.coroutines.launch
 public fun MapView(
     modifier: DisplayModifier,
     mapTilerSecretKey: String,
-    latitude: Double = 0.0,
-    longitude: Double = 0.0,
-    startScale: Double = 1.0,
+    latitude: Double? = null,
+    longitude: Double? = null,
+    startScale: Double? = null,
+    externalState: MutableState<ExternalState> = remember {
+        mutableStateOf(
+            ExternalState(
+                latitude ?: 0.0,
+                longitude ?: 0.0,
+                startScale ?: 0.0
+            )
+        )
+    },
+    onStateChange: (ExternalState) -> Unit = { externalState.value = it },
     onMapViewClick: (latitude: Double, longitude: Double) -> Boolean = { lat, lon -> true },
 ) {
     val viewScope = rememberCoroutineScope()
     val ioScope = remember { CoroutineScope(SupervisorJob(viewScope.coroutineContext.job) + getDispatcherIO()) }
     val imageRepository = rememberTilesRepository(ioScope, mapTilerSecretKey)
+
+    var width: Int by remember { mutableStateOf(100) }
+    var height: Int by remember { mutableStateOf(100) }
+    var cache: Map<Tile, TileImage> by remember { mutableStateOf(mapOf()) }
+    val displayTiles: List<DisplayTileWithImage<TileImage>> by derivedStateOf {
+        ioScope.launch {
+            //todo load tile
+        }
+        externalState.value
+    }
+
     val mapStore: Store<MapState<TileImage>, MapIntent<TileImage>> = remember {
         viewScope.createMapStore(
-            latitude = latitude,
-            longitude = longitude,
-            startScale = startScale,
+            latitude = externalState.value.latitude,
+            longitude = externalState.value.longitude,
+            startScale = externalState.value.scale,
             searchOrCropOrNull = { searchOrCrop(it) },
         ) { store, sideEffect ->
             when (sideEffect) {
